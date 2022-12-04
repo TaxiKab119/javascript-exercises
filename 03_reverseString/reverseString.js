8a8d1fcb65a044dac184829e13b179ce0911cd33
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2022-12-04 15:15:23 -0500

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let reversedString = '';
+    let index = input.length - 1;
+    for(let i = 1;i <= input.length;i++){
+        reversedString += input[index];
+        index -= 1;
+    }
+    return reversedString
 
 };
 
