5a0d25dea2b3d026b796be090dc8d7ef28b751d6
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2022-12-04 16:36:41 -0500

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray,...removePlease) {
+    
+    for(let i = 0;i < removePlease.length; i++){
+        let arrayIndex = 0;
+        do{
+            if (inputArray[arrayIndex] === removePlease[i]){
+                inputArray.splice(arrayIndex,1);
+            } else {
+                arrayIndex +=1;
+            }
+        } while(arrayIndex < inputArray.length)
+    }
+    return inputArray
 };
 
 // Do not edit below this line
