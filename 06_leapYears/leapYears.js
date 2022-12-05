ef4d7680b0b30eb13d3a8600c2dddef420b7791f
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2022-12-04 23:17:10 -0500

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap years are divisible by four
+    // *however* years divisible by 100 are not leap years UNLESS they are divisible by 400
 
+    if ((year % 4 == 0) && (year % 100 != 0)){
+        return true
+    } else if  (year % 400 == 0){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
